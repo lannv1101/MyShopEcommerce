e20489825efb323cd835137ecd680c0fb38bbec6
Lan Vu Nguyen <nguyenvulan1998@gmail.com>
2022-04-11 15:34:31 +0700

    ProductManager Build And Test

--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -8,65 +8,74 @@ using MyShop.Core.Models;
 
 namespace MyShop.DataAccess.InMemory
 {
-    internal class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
-        List<Product> products ;
+        List<Product> products;
 
         public ProductRepository()
         {
             products = cache["products"] as List<Product>;
-            if (products==null)
+            if (products == null)
             {
                 products = new List<Product>();
             }
         }
+
         public void Commit()
         {
             cache["products"] = products;
         }
+
         public void Insert(Product p)
         {
             products.Add(p);
         }
+
         public void Update(Product product)
         {
             Product productToUpdate = products.Find(p => p.Id == product.Id);
-            if (productToUpdate!=null)
+
+            if (productToUpdate != null)
             {
                 productToUpdate = product;
             }
             else
             {
-                throw new Exception("Product Not Found");
-            } 
+                throw new Exception("Product no found");
+            }
         }
+
         public Product Find(string Id)
         {
             Product product = products.Find(p => p.Id == Id);
+
             if (product != null)
             {
-               return product;
+                return product;
             }
             else
             {
-                throw new Exception("Product Not Found");
+                throw new Exception("Product no found");
             }
         }
+
         public IQueryable<Product> Collections()
         {
             return products.AsQueryable();
         }
+
         public void Delete(string Id)
         {
             Product productToDelete = products.Find(p => p.Id == Id);
+
             if (productToDelete != null)
             {
                 products.Remove(productToDelete);
             }
             else
             {
-                throw new Exception("Product Not Found");
+                throw new Exception("Product no found");
             }
         }
     }
