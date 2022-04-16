5a817f6c73495d0f244736c6729fa72981ae5ece
Lan Vu Nguyen <nguyenvulan1998@gmail.com>
2022-04-16 21:44:40 +0700

    added Product Listing

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +9,32 @@ using System.Web.Mvc;
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
+
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> producContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            this.context = producContext;
+            this.productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collections().ToList();   
+            return View(products);
+        }
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
